f3676386bbccfc15c6aa733006a1f92b99853ffb
Oscar.Quesada <quesadaao@hotmail.com>
2020-11-04 20:56:13 -0600

    Semana 8

---
+++ SC701-P2/Solution.UI.Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Solution.UI.Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
